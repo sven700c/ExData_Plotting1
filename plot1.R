5e6890fac84fd6171194a20c44b9205cfebd3412
unknown <svens@sinwwp2110.corp.tojigroup.com>
2014-07-10 15:18:11 +0800

    added sql

--- plot1.R
+++ plot1.R
@@ -1,6 +1,14 @@
+if(!require('sqldf')){
+    install.packages('sqldf')
+}
+
+library(sqldf)
+
 colclasses = c("character", "character", rep("numeric",7))
-data <- read.csv("household_power_consumption.txt", colClasses = colclasses, 
-                 na.strings= "?", sep = ";")
+sql <- "SELECT * FROM file WHERE Date='1/2/2007' OR Date='2/2/2007'"
+data <- read.csv.sql("household_power_consumption.txt", sql, sep=';',
+                        colClasses=colclasses, header = T)
+
 
 hist(data$Global_active_power, col = "red", main = "Global Active Power",
      xlab = "Global Active Power(kilowatt)")
